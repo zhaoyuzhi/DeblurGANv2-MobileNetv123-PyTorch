9bab2684666b16ed89b3e4052e5d5df7be34c399
Yuzhi ZHAO <yzzhao2-c@my.cityu.edu.hk>
2020-05-18 11:36:09 +0800

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,110 @@
 # DeblurGANv2-PyTorch
-Provide pre-trained FPN models for DeblurGANv2
+
+## 1 Training
+
+- Python 3.6
+- PyTorch 1.0 with Cuda 8.0
+- OpenCV
+
+## 2 Training
+
+Please change the path parameter to your dataset path. Then run `train_pre.sh` or `train_all.sh`:
+
+```bash
+sh train_pre.sh
+or
+sh train_all.sh
+```
+
+## 3 Validation
+
+```bash
+python validation.py
+```
+
+## 4 Pre-trained models
+
+[Onedrive link](https://portland-my.sharepoint.com/:f:/g/personal/yzzhao2-c_ad_cityu_edu_hk/EvghwTdReyxIl8ccqJc6IVIBwSZXxxTHBXoNuab1M3d1Nw?e=5nHvyX) includes logs, models, and trained_models.
+
+## 5 Samples
+
+input-generated-ground truth; input-generated-ground truth
+
+<img src="./img/val_epoch213_in.png" width="150"/>
+<img src="./img/val_epoch213_pred.png" width="150"/>
+<img src="./img/val_epoch213_gt.png" width="150"/>
+<img src="./img/val_epoch214_in.png" width="150"/>
+<img src="./img/val_epoch214_pred.png" width="150"/>
+<img src="./img/val_epoch214_gt.png" width="150"/>
+
+input-generated-ground truth; input-generated-ground truth
+
+<img src="./img/val_epoch215_in.png" width="150"/>
+<img src="./img/val_epoch215_pred.png" width="150"/>
+<img src="./img/val_epoch215_gt.png" width="150"/>
+<img src="./img/val_epoch241_in.png" width="150"/>
+<img src="./img/val_epoch241_pred.png" width="150"/>
+<img src="./img/val_epoch241_gt.png" width="150"/>
+
+input-generated-ground truth; input-generated-ground truth
+
+<img src="./img/val_epoch237_in.png" width="150"/>
+<img src="./img/val_epoch237_pred.png" width="150"/>
+<img src="./img/val_epoch237_gt.png" width="150"/>
+<img src="./img/val_epoch238_in.png" width="150"/>
+<img src="./img/val_epoch238_pred.png" width="150"/>
+<img src="./img/val_epoch238_gt.png" width="150"/>
+
+input-generated-ground truth; input-generated-ground truth
+
+<img src="./img/val_epoch254_in.png" width="150"/>
+<img src="./img/val_epoch254_pred.png" width="150"/>
+<img src="./img/val_epoch254_gt.png" width="150"/>
+<img src="./img/val_epoch255_in.png" width="150"/>
+<img src="./img/val_epoch255_pred.png" width="150"/>
+<img src="./img/val_epoch255_gt.png" width="150"/>
+
+input-generated-ground truth; input-generated-ground truth
+
+<img src="./img/val_epoch256_in.png" width="150"/>
+<img src="./img/val_epoch256_pred.png" width="150"/>
+<img src="./img/val_epoch256_gt.png" width="150"/>
+<img src="./img/val_epoch257_in.png" width="150"/>
+<img src="./img/val_epoch257_pred.png" width="150"/>
+<img src="./img/val_epoch257_gt.png" width="150"/>
+
+input-generated-ground truth; input-generated-ground truth
+
+<img src="./img/val_epoch265_in.png" width="150"/>
+<img src="./img/val_epoch265_pred.png" width="150"/>
+<img src="./img/val_epoch265_gt.png" width="150"/>
+<img src="./img/val_epoch266_in.png" width="150"/>
+<img src="./img/val_epoch266_pred.png" width="150"/>
+<img src="./img/val_epoch266_gt.png" width="150"/>
+
+input-generated-ground truth; input-generated-ground truth
+
+<img src="./img/val_epoch288_in.png" width="150"/>
+<img src="./img/val_epoch288_pred.png" width="150"/>
+<img src="./img/val_epoch288_gt.png" width="150"/>
+<img src="./img/val_epoch289_in.png" width="150"/>
+<img src="./img/val_epoch289_pred.png" width="150"/>
+<img src="./img/val_epoch289_gt.png" width="150"/>
+
+input-generated-ground truth; input-generated-ground truth
+
+<img src="./img/val_epoch297_in.png" width="150"/>
+<img src="./img/val_epoch297_pred.png" width="150"/>
+<img src="./img/val_epoch297_gt.png" width="150"/>
+<img src="./img/val_epoch298_in.png" width="150"/>
+<img src="./img/val_epoch298_pred.png" width="150"/>
+<img src="./img/val_epoch298_gt.png" width="150"/>
+
+input-generated-ground truth; input-generated-ground truth
+
+<img src="./img/val_epoch299_in.png" width="150"/>
+<img src="./img/val_epoch299_pred.png" width="150"/>
+<img src="./img/val_epoch299_gt.png" width="150"/>
+<img src="./img/val_epoch300_in.png" width="150"/>
+<img src="./img/val_epoch300_pred.png" width="150"/>
+<img src="./img/val_epoch300_gt.png" width="150"/>
--- validation.py
+++ validation.py
@@ -13,26 +13,32 @@ if __name__ == "__main__":
     # ----------------------------------------
     parser = argparse.ArgumentParser()
     # Pre-train, saving, and loading parameters
-    parser.add_argument('--pre_train', type = bool, default = False, help = 'pre_train or not')
-    parser.add_argument('--load_name', type = str, default = './track1/G_epoch8000_bs8.pth', \
-            help = 'load the pre-trained model with certain epoch, None for pre-training')
-    parser.add_argument('--test_batch_size', type = int, default = 1, help = 'size of the testing batches for single GPU')
-    parser.add_argument('--num_workers', type = int, default = 2, help = 'number of cpu threads to use during batch generation')
-    parser.add_argument('--val_path', type = str, default = './validation', help = 'saving path that is a folder')
-    parser.add_argument('--task_name', type = str, default = 'track1', help = 'task name for loading networks, saving, and log')
-    # Network initialization parameters
+    parser.add_argument('--pre_train', type = bool, default = False, help = 'pre-train ot not')
+    parser.add_argument('--load_name', type = str, default = '', help = 'load the pre-trained model with certain epoch')
+    parser.add_argument('--test_batch_size', type = int, default = 1, help = 'size of the batches')
+    parser.add_argument('--num_workers', type = int, default = 1, help = 'number of cpu threads to use during batch generation')
+    parser.add_argument('--save_path', type = str, default = '', help = 'the save path to generated images')
+    # Initialization parameters
+    parser.add_argument('--network_type', type = str, default = 'MobileNetv3_small', help = 'pad type of networks')
     parser.add_argument('--pad', type = str, default = 'reflect', help = 'pad type of networks')
     parser.add_argument('--activ_g', type = str, default = 'relu', help = 'activation type of generator')
     parser.add_argument('--activ_d', type = str, default = 'lrelu', help = 'activation type of discriminator')
-    parser.add_argument('--norm', type = str, default = 'in', help = 'normalization type of networks')
+    parser.add_argument('--norm', type = str, default = 'bn', help = 'normalization type of networks')
     parser.add_argument('--in_channels', type = int, default = 3, help = '1 for colorization, 3 for other tasks')
     parser.add_argument('--out_channels', type = int, default = 3, help = '2 for colorization, 3 for other tasks')
-    parser.add_argument('--start_channels', type = int, default = 64, help = 'start channels for the main stream of generator')
+    parser.add_argument('--filter_channels', type = int, default = 128, help = 'start channels for generator')
+    parser.add_argument('--mid_channels', type = int, default = 64, help = 'start channels for generator')
+    parser.add_argument('--start_channels', type = int, default = 64, help = 'start channels for discriminator')
     # Dataset parameters
-    parser.add_argument('--baseroot_A', type = str, default = '', help = 'blurry image baseroot')
-    parser.add_argument('--baseroot_B', type = str, default = '', help = 'clean image baseroot')
-    parser.add_argument('--crop_size', type = int, default = 256, help = 'crop size for each image')
+    parser.add_argument('--baseroot_test_blur', type = str, \
+        default = 'E:\\dataset, task related\\Deblurring Dataset\\GOPRO\\GOPRO_3840FPS_AVG_3-21\\test\\blur', \
+            help = 'blurry image baseroot')
+    parser.add_argument('--baseroot_test_sharp', type = str, \
+        default = 'E:\\dataset, task related\\Deblurring Dataset\\GOPRO\\GOPRO_3840FPS_AVG_3-21\\test\\sharp', \
+            help = 'clean image baseroot')
+    parser.add_argument('--crop_size', type = int, default = 0, help = 'crop size for each image')
     opt = parser.parse_args()
+    print(opt)
 
     # ----------------------------------------
     #                   Test
@@ -41,8 +47,7 @@ if __name__ == "__main__":
     generator = utils.create_generator(opt).cuda()
     test_dataset = dataset.DeblurDataset_val(opt)
     test_loader = torch.utils.data.DataLoader(test_dataset, batch_size = opt.test_batch_size, shuffle = False, num_workers = opt.num_workers, pin_memory = True)
-    sample_folder = os.path.join(opt.val_path, opt.task_name)
-    utils.check_path(sample_folder)
+    utils.check_path(opt.save_path)
 
     # forward
     for i, (true_input, true_target, imgname) in enumerate(test_loader):
@@ -58,5 +63,5 @@ if __name__ == "__main__":
         # Save
         fake_target = fake_target.clone().data.permute(0, 2, 3, 1)[0, :, :, :].cpu().numpy()
         fake_target = cv2.cvtColor(fake_target, cv2.COLOR_BGR2RGB).astype(np.uint8)
-        save_img_path = os.path.join(sample_folder, imgname)
+        save_img_path = os.path.join(opt.save_path, imgname)
         
